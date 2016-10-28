8afecde9774b4d07d8cc300c29e335f100500287
ruy <ruygsgoliveira@live.com>
2016-10-28 15:46:35 -0300

    verse final

---
+++ Assets/Scripts/Cano.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Cano : MonoBehaviour {
+    public float velocidade = 2.0f;
+    public float maxY;
+    public float minY;
+    public float posX;
+    public float intervalo;
+    public float cronometro;
+    public int pontuacao;
+    public Text textoPontuacao;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+        transform.Translate(Vector2.left * velocidade * Time.deltaTime);
+        cronometro = cronometro + Time.deltaTime;
+        if (cronometro > intervalo)
+        {
+            pontuacao++;
+            textoPontuacao.text = pontuacao.ToString();
+            Vector2 novaPosicao;
+            novaPosicao.x = posX;
+            novaPosicao.y = Random.Range(minY,maxY);
+            transform.position = novaPosicao;
+            cronometro = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Flappy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flappy : MonoBehaviour {
+    public Rigidbody2D _rigid;
+    public float velocidade = 3.0f;
+    // verifica colisao
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        // reinicia o jogo
+        Application.LoadLevel(0);
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rigid.velocity = Vector2.up * velocidade;
+        }
+	}
+}
